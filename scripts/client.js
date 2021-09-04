b87d06b717dca5c3bdd198eed962fa30fb8ac72d
Jennifer Alexander <jenny_alexander@icloud.com>
2021-09-03 19:07:29 -0500

    add <p> DOM element for result text

--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,8 @@
-//js handshake
-console.log(`js`);
-
-//JQuery handshake
-$( document ).ready( onReady );
-
-/* Instructions
-Write a program that takes in a year (as an integer) and returns true if the 
-year is a leap year. Years that are divisible by 4 are leap years, but years 
-that are divisible by 100 are not leap years, but years that are divisible 
-by 400 are leap years.
+/* Instructions:
+Write a program that takes in a year (as an integer) and returns true if the year is a leap year. 
+Century years (divisile by 100) are usually leap years except if they are also divisibile by 400. 
+Then they are not a leap year.
+If not a century year, then if year is divisible by 4, year is a leap year.
 */
 /* Test data
 1999 -> false (normal year)
@@ -17,46 +11,35 @@ by 400 are leap years.
 2000 -> true (divisible by 400)
 */
 
-function onReady() {
-    console.log(`JQ`);
+//JQuery handshake
+$( document ).ready( onReady );
 
+function onReady() {
+ 
     $( "#checkYearBtn").on( 'click', checkYear );
 }
 
-function checkYear() {
+function checkYear( ) {
     let elYear = $( '#year' ).val();
-    console.log(elYear);
+    let returnText;
 
 //Checking if year is a century and then check for div by 400
     if ( elYear % 100 === 0 && elYear % 400 != 0 ) {
-        console.log("this is not a leap year");
-        return false;    
+        returnText = "This is not a leap year!"; 
     } else {
         if ( elYear % 4 === 0 ) {
-            console.log("this is a leap year");
-            return true;
+            returnText = "This is a leap year!"; 
         } else {
-            console.log("this is not a leap year");
-            return false;
+            returnText = "This is not a leap year!"; 
         }
     }
+
+    console.log(returnText);
+    //Output the answer to the DOM
+    //Get the main container
+    let elMain = $( '.container' );
+    //Dynamically create the p tag to hold the result
+    elMain.append('<p>' + returnText + '</p>');
+
 }
-// //1st check if the year is a century year
-// if ( elYear % 100 === 0 ) {
-//     if ( elYear % 400 === 0 ) {
-//         console.log("this is a leap year");
-//         return true;
-//     } else {
-//         console.log("this is not a leap year");
-//         return false;
-//     }
-// //not a century year so do the 'regular' check if divisible by 4
-// } else {
-//     if ( elYear % 4 === 0 ) {
-//         console.log("this is a leap year");
-//         return true;
-//     } else {
-//         console.log("this is not a leap year");
-//         return false;
-//     }
-// }
+
